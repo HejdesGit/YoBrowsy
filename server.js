868d8c27d5843dbebfd264dc170feefeecebefc7
Johan <hejdes@gmail.com>
2014-09-05 23:20:04 +0200

    no message

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-'use strict';
-
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.redirect("/index.html");
+  response.send('Hello World!')
 });
 
 app.listen(app.get('port'), function() {
