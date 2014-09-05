7dd6e7351d928994e3393fb6ef349d4a861906fc
Johan <hejdes@gmail.com>
2014-09-05 22:52:17 +0200

    no message

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/dist'))
 
 app.get('/', function(request, response) {
-  res.redirect("/index.html");
-})
+  request.redirect("/index.html");
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
